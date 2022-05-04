b57d429354f4f8567aeca5f8fa177748a6498a25
Suporte <junioralbino741@gmail.com>
2022-05-04 14:43:51 -0300

    Colocando o tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
-interface ButtonProps {
-    text?: string;
-}
+import { Widget } from "./components/Widget";
+import {ChatTeardropDots} from 'phosphor-react';
 
-function Button(props: ButtonProps){
-   return <button className="bg-violet-500">{props.text ?? 'Default'}</button>
-}
+export function App(){
+  return(
+    <div className="absolute bottom-4 right-4">
+      <button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+         <ChatTeardropDots className="w-6 h-6"/>
 
-
-function App() {
-  return (
-    <div>
-      <Button text="Enviar" />
-      <Button text="Fale conosco" />
-      <Button />
-    </div>
+         <span className="max-w-0 overflow-hidden group-hover:max-w-xs">
+           <span className="pl-2"></span>
+           Feedback
+         </span>
+      </button>
+    </div>   
   )
 }
-
-export default App
