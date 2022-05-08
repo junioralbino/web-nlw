e69252a5b657a31092bc521166570037abe3d54d
junior <junioralbino741@gmail.com>
2022-05-08 18:09:22 -0300

    Finalindo o formulario

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,5 +1,39 @@
-export function FeedbackContentStep(){
+import { ArrowLeft } from "phosphor-react";
+import { FeedbackType, feedbackTypes } from "..";
+import { CloseButton } from "../../CloseButton";
+
+interface FeedbackContentStepProps {
+    feedbackType: FeedbackType;
+    onFeedbackRestartRequested: () => void;
+}
+
+export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }:  FeedbackContentStepProps){
+
+    const feedbackTypesInfo = feedbackTypes[feedbackType];
+
     return(
-        <h1>FeedbackContentStep</h1>
+      <>
+        <header>
+            
+            <button type="button" className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
+             onClick={onFeedbackRestartRequested}>
+              <ArrowLeft  weight="bold" className="w-4 h-4"  />
+            </button>
+
+            <span className="text-xl leading-6 flex items-center gap-2">
+                <img src={feedbackTypesInfo.image.source} alt={feedbackTypesInfo.image.alt} className="w-6 h-6" />
+                {feedbackTypesInfo.title}
+            </span>
+
+            <CloseButton  />
+        </header>
+
+        <form className="my-4 w-full">
+           <textarea 
+            className="min-w-[384px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600  bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
+            placeholder="Conte com detalhes o que está acontecendo..."
+           />
+        </form>
+      </>  
     );  
  }
--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -4,8 +4,8 @@ import bugImageUrl  from '../../assets/bug.svg';
 import ideaImageUrl  from '../../assets/idea.svg';
 import thoughtImageUrl  from '../../assets/thought.svg';
 
-import { FeedbackTypeStep } from "./Steps/feedbackTypeStep";
-import { FeedbackContentStep } from "./Steps/feedbackContentStep";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
 
 
 export const feedbackTypes = {
@@ -38,13 +38,17 @@ export type FeedbackType = keyof typeof feedbackTypes;
 export function WidgetForm(){
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
 
+  function handleRestarFeedback(){
+    setFeedbackType(null)
+  }
+
     return(
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
         
         { !feedbackType ? (
           <FeedbackTypeStep  onFeedbackTypeChanged={setFeedbackType} />
         ) : (
-           <FeedbackContentStep />
+           <FeedbackContentStep  feedbackType={feedbackType} onFeedbackRestartRequested={handleRestarFeedback} />
         )} 
 
           <footer className="text-xs text-neutral-400">
