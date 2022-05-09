833a1fd19c9e952ae54243a7ac741503b0081986
Suporte <junioralbino741@gmail.com>
2022-05-09 13:18:20 -0300

    finalizando o icones de screenshot

--- src/components/WidgetForm/ScreenshotButton.tsx
+++ src/components/WidgetForm/ScreenshotButton.tsx
@@ -4,8 +4,8 @@ import { useState } from "react";
 import { Loading } from "../Loading";
 
 interface ScreenshotButtonProps {
-   screenshot: string;
-   onScreenshotTook: (screenshot: string) => void;
+   screenshot: string | null;
+   onScreenshotTook: (screenshot: string | null) => void;
 }
 
 export function ScreenshotButton({screenshot, onScreenshotTook }: ScreenshotButtonProps){
@@ -26,6 +26,7 @@ export function ScreenshotButton({screenshot, onScreenshotTook }: ScreenshotButt
     if(screenshot){
        return(
         <button type="button" className="p-1 w-10 h-10 rounded-md border-transparent flex justify-end items-end text-zinc-400 hover:text-zinc-100 transition-colors"
+        onClick={ () => onScreenshotTook(null) }
         style={{
           backgroundImage: `url(${screenshot})`,
           backgroundPosition: "right bottom",
