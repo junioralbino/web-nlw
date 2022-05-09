039fd37fa504c24469795e87482a865ba3433402
Suporte <junioralbino741@gmail.com>
2022-05-09 16:18:59 -0300

    finalizado aula 2

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -7,9 +7,10 @@ import { ScreenshotButton } from "../ScreenshotButton";
 interface FeedbackContentStepProps {
     feedbackType: FeedbackType;
     onFeedbackRestartRequested: () => void;
+    onFeedbackSent: () => void;
 }
 
-export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }:  FeedbackContentStepProps){
+export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested, onFeedbackSent  }:  FeedbackContentStepProps){
      
     const [screenshot,  setScreenshot] = useState<string | null>(null)
     const [comment, setComment] =  useState('');
@@ -18,7 +19,9 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }
 
     function handleSubmitFeedback(event: FormEvent){
           event.preventDefault();
-          console.log(screenshot, comment)
+          console.log({screenshot, comment})
+
+          onFeedbackSent();
     }
 
 
@@ -53,7 +56,7 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }
                onScreenshotTook={setScreenshot}
              />
 
-              <button type="submit"disabled={comment.length === 0} className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 hover:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50">
+              <button type="submit"disabled={comment.length === 0} className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 hover:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500">
                      Enviar feedback
               </button>
            </footer>
--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -6,6 +6,7 @@ import thoughtImageUrl  from '../../assets/thought.svg';
 
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSucessStep } from "./Steps/FeedbackSucessStep";
 
 
 export const feedbackTypes = {
@@ -36,20 +37,31 @@ export type FeedbackType = keyof typeof feedbackTypes;
 
 
 export function WidgetForm(){
-  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSent,  setFeebackSent] = useState(false);
 
   function handleRestarFeedback(){
-    setFeedbackType(null)
+    setFeebackSent(false);
+    setFeedbackType(null);
   }
 
     return(
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-        
-        { !feedbackType ? (
-          <FeedbackTypeStep  onFeedbackTypeChanged={setFeedbackType} />
-        ) : (
-           <FeedbackContentStep  feedbackType={feedbackType} onFeedbackRestartRequested={handleRestarFeedback} />
-        )} 
+         {feedbackSent ? (
+           <FeedbackSucessStep onFeedbackRestartRequested={handleRestarFeedback} />
+           ): (
+            <>
+                { !feedbackType ? (
+                  <FeedbackTypeStep  onFeedbackTypeChanged={setFeedbackType} />
+                ) : (
+                  <FeedbackContentStep  
+                    feedbackType={feedbackType} 
+                    onFeedbackRestartRequested={handleRestarFeedback}
+                    onFeedbackSent={() => setFeebackSent(true)}
+                    />
+                  )}
+              </> 
+          ) }
 
           <footer className="text-xs text-neutral-400">
               Feito com ♥ pela <a className="underline underline-offset-2" href="https://rocketseat.com.br/">Rocketseat</a>
