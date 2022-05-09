7d5476268bdecff07e7b55cc1192e22c7abaa302
Suporte <junioralbino741@gmail.com>
2022-05-09 08:52:29 -0300

    Finalizado o loading do upload

--- src/components/WidgetForm/ScreenshotButton.tsx
+++ src/components/WidgetForm/ScreenshotButton.tsx
@@ -1,16 +1,24 @@
 import html2canvas from "html2canvas";
 import { Camera } from "phosphor-react";
+import { useState } from "react";
+import { Loading } from "../Loading";
 
 export function ScreenshotButton(){
+
+    const [isTakingScreenshot, setIsTakingScreenshot] = useState(false)
+
     async function handleTakeScreenshot(){
+       setIsTakingScreenshot(true);
+
        const canvas =  await html2canvas(document.querySelector('html')!);
        const base64image = canvas.toDataURL('image/png');
 
        console.log(base64image)
+       setIsTakingScreenshot(false);
     }
     return(
       <button type="button" onClick={handleTakeScreenshot} className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors">
-        <Camera className="w-6 h-6" />    
+        { isTakingScreenshot ? <Loading /> : <Camera className="w-6 h-6" />  }   
       </button>
     );
 }
