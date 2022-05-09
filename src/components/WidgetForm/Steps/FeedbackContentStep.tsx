3caa7e6a495bd10bcfc5d4323d2915b25978866c
Suporte <junioralbino741@gmail.com>
2022-05-09 16:21:47 -0300

    finalizado aula 2

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -56,7 +56,7 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested, 
                onScreenshotTook={setScreenshot}
              />
 
-              <button type="submit"disabled={comment.length === 0} className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 hover:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500">
+              <button type="submit"disabled={comment.length === 0} className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 hover:outline-none focus:ring-2 focus:ring-offset-2  transition-colors disabled:opacity-50 disabled:hover:bg-brand-500">
                      Enviar feedback
               </button>
            </footer>
--- src/components/WidgetForm/Steps/FeedbackSucessStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackSucessStep.tsx
@@ -19,7 +19,7 @@ export function FeedbackSucessStep({onFeedbackRestartRequested}: FeedbackSucessS
           
           <button
            type="button"
-           className="py-2 px-6 mt-6 bg-zinc-800 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 transition-colors hover:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-zinc-900 focus:ring-brand-500  disabled:opacity-50 disabled:hover:bg-brand-500"
+           className="py-2 px-6 mt-6 bg-zinc-800 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 transition-colors hover:outline-none focus:ring-2 focus:ring-offset-2   disabled:opacity-50 disabled:hover:bg-brand-500"
            onClick={onFeedbackRestartRequested}
           >
                 Quero enviar outro
